37462660afcca97e1c4f6bc90b241aba8b7b0f51
Марина Миловкина <un6un5@gmail.com>
2025-04-13 16:39:28 +0300

    Добавлен цикл вÑл, выводящий четные числа от 1 до 10

--- src/Count.java
+++ src/Count.java
@@ -7,15 +7,24 @@ public class Count {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите первое число: ");
-            int number1 = scanner.nextInt(); // Читаем первое число
+        int number1 = scanner.nextInt(); // Читаем первое число
 
         System.out.println("Введите второе число: ");
-            int number2 = scanner.nextInt(); // Читаем второе число
+        int number2 = scanner.nextInt(); // Читаем второе число
 
         int sum = number1 + number2; // Складываем числа
 
         System.out.println("Сумма чисел: " + sum);
 
         scanner.close(); // Закрываем сканнер
+
+
+        // вывод четных чисел от 1 до 10 через цикл
+        System.out.println("Четные числа от 1 до 10:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) { // проверка на четность
+                System.out.println(i);
+            }
+        }
     }
 }
