73d2c717ec6f291629d6b4a6e4d64446d9978901
Марина Миловкина <un6un5@gmail.com>
2025-04-13 16:28:50 +0300

    Создан новый класс count

---
+++ src/Count.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+        // Создаем объект Scanner для ввода данных
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+            int number1 = scanner.nextInt(); // Читаем первое число
+
+        System.out.println("Введите второе число: ");
+            int number2 = scanner.nextInt(); // Читаем второе число
+
+        int sum = number1 + number2; // Складываем числа
+
+        System.out.println("Сумма чисел: " + sum);
+
+        scanner.close(); // Закрываем сканнер
+    }
+}
